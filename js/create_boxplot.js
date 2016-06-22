c4f8943bec31046f40e5c387243461e52f346e09
zelite <jcafernandes@gmail.com>
2016-06-22 11:28:09 +0200

    boxes and median
    roll my own boxplot

--- js/create_boxplot.js
+++ js/create_boxplot.js
@@ -1,60 +1,103 @@
-// Used https://bl.ocks.org/mbostock/4061502 for guidance
-function iqr(k) {
-  //function from https://bl.ocks.org/mbostock/4061502
-  //returns function that calculates interquartile range.
-  return function(d, i) {
-    var q1 = d.quartiles[0],
-        q3 = d.quartiles[2],
-        iqr = (q3 - q1) * k,
-        i = -1,
-        j = d.length;
-    while (d[++i] < q1 - iqr);
-    while (d[--j] > q3 + iqr);
-    return [i, j];
-  };
-}
-
 function draw_box_plot(data, v_name){
-  /*var boxPlot = d3.select("body").append("svg")
+  data = data.sort(function(a, b){
+    return a[v_name] - b[v_name];
+  });
+
+  var chart = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height+margin.top+margin.bottom)
-        .attr("class", "boxplot")
+        .attr("class", "boxplot "+v_name)
       .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");*/
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var margin = {top: 20, right: 30, bottom:20, left:30};
+  var byHand = d3.nest()
+    .key(function(d){
+      return d.handedness;
+    })
+    .entries(data);
 
-var width = 120 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  /* d3.quantile does not work with an acessor function
+  therefore the values need to be extracted to an array and
+  sorted beforehand*/
+  var summaries = d3.nest()
+    .key(function(d){
+      return d.handedness;
+    })
+    .rollup(function(leaves){
+      var v_array = leaves.map(function(d){return d[v_name];});
+      var summary = {"min": d3.min(v_array),
+              "q1": d3.quantile(v_array, 0.25),
+              "median": d3.median(v_array),
+              "q3": d3.quantile(v_array, 0.75),
+              "max": d3.max(v_array)
+            };
+      summary.iqr = summary.q3 - summary.q1;
+      summary.topWhisker = summary.q3 + 1.5*summary.iqr;
+      summary.bottomWhisker = summary.q1 - 1.5*summary.iqr;
+      return summary;
+      })
+    .entries(data);
 
-  var min = d3.min(data, function(d) {return d[v_name];}),
-      max = d3.max(data, function(d) {return d[v_name];});
-  //debugger;
+  var x = d3.scale.ordinal()
+          .domain(["R", "L", "B"])
+          .rangeBands([0, width], 0.1);
 
-  //Preparing the data to be used by the box plugin
-  var var_data = [[], [], []];
-  data.forEach(function(x){
-    var v = x[v_name];
-    var hand = x.handedness;
-    var hands = {"R": 0, "L": 1, "B": 2};
-    var_data[hands[hand]].push(v);
-  });
-  console.log(var_data);
+  var y = d3.scale.linear()
+            .domain([0, d3.max(summaries, function(d) {return d.values.max;})])
+            .range([height, 0]);
 
-  var boxplot = d3.box()
-    .whiskers(iqr(1.5))
-    .width(width)
-    .height(height);
+  //Add axis
+  var hand_axis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(function(value){//replace RLB with longer labels
+      new_labels = {"R": "Right-handed Players",
+                    "L": "Left-handed Players",
+                    "B": "Ambidextrous Players"};
+      return new_labels[value];
+    });
 
-  boxplot.domain([min, max]);
+  var count_axis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
 
-  var svg = d3.select("body").selectAll("svg .box"+" ."+v_name)
-      .data(var_data)
-    .enter().append("svg")
-      .attr("class", "box"+" "+v_name)
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.bottom + margin.top)
-    .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top+")")
-      .call(boxplot);
+  chart.append("g")
+    .attr("class", "hand axis")
+    .attr("transform", "translate(0,"+ height+")")
+    .call(hand_axis);
+
+  chart.append("g")
+    .attr("class", "count axis")
+    .call(count_axis);
+
+  //draw boxes  - very similar
+  //to boxplot, just the y and height are different.
+  var box = chart.selectAll(".box")
+      .data(summaries)
+    .enter().append("rect")
+    //put the boxes on the x axis
+      .attr("class", "box")
+      .attr("x", function(d){return x(d.key);})
+      .attr("width", x.rangeBand())
+      .attr("y", function(d){return y(d.values.q3);})
+      .attr("height", function(d){return y(d.values.q1)-y(d.values.q3);});
+
+  //draw median line
+  var line = chart.selectAll(".median")
+      .data(summaries)
+    .enter().append("line")
+      .attr("class", "median")
+      .attr("x1", function(d){return x(d.key);})
+      .attr("x2", function(d){return x(d.key)+x.rangeBand();})
+      .attr("y1", function(d){return y(d.values.median);})
+      .attr("y2", function(d){return y(d.values.median);})
+      .style("stroke", "red");
+
+  //Select Outliers
+  //
+
+
+
+
+
 }
